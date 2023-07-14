06845574b277c143daa57d4c06b48768c95d7405
Kendall <kendallawills@gmail.com>
2023-07-14 16:02:00 -0400

    Return

--- main.js
+++ main.js
@@ -23,9 +23,18 @@ function sayThankYou(name) {
 
 sayThankYou('Cole');
 
-//Default Parameters
+// Default Parameters
 function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
     console.log(`Remember to buy ${item1}`);
     console.log(`Remember to buy ${item2}`);
     console.log(`Remember to buy ${item3}`);
 }
+
+// Return
+function monitorCount(rows, columns) {
+    return rows * columns;
+}
+
+const numOfMonitors = monitorCount(5, 4);
+
+console.log(numOfMonitors);
